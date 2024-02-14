194e03c2a3dcce075b0c1429bd61e99e6eecd400
Johan Ballesteros <johan.ballesteros@outlook.com>
2024-02-14 16:07:41 +0000

    Test

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-def hello():
-    try:
-        print(x)
-    except:
-        print("Something went wrong")
-    finally:
-        print("The 'try except' is finished")
-        return
+def is_normal(f):
+    return f == f
 
-hello()
+print(is_normal(2))
